e71998e5c4e3c5fb889a1f88093d51b1c1868261
Rihanrh <rihanh@student.telkomuniversity.ac.id>
2024-10-18 22:56:20 +0700

    Comment fixes

--- src/utils/retrieveContent.js
+++ src/utils/retrieveContent.js
@@ -2,7 +2,7 @@ const { openai } = require("./openaiClient");
 const fs = require("fs");
 
 /**
- * Writes the content of the OpenAI file to a local file.
+ * Writes the content of the OpenAI file to a new variable.
  * 
  * @param {string} filePath - The path of the file to retrieve content from.
  */
