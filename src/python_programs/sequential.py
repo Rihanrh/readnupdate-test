8fef9f2c6bcf29a2a82a7d158a98527b32008575
Rihanrh <rihanh@student.telkomuniversity.ac.id>
2025-01-17 21:13:52 +0700

    Update sequential.py

--- src/python_programs/sequential.py
+++ src/python_programs/sequential.py
@@ -1,6 +1,10 @@
-
 def search(x, seq):
-    for i, e in enumerate(seq):
-        if x <= e:
-            return i
-    return len(seq)
+    if x < seq[0]:
+        return 0
+    elif x > seq[-1]:
+        return len(seq)
+    else:
+        seq_enum = [i for i in enumerate(seq)]
+        for j in range(len(seq_enum) - 1):
+            if x >= seq_enum[j][1] and x <= seq_enum[j+1][1]:
+                return j+1
